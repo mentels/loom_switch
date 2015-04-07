5db20ecbfd7d83fb783d4c006c569befbeffd169
Szymon Mentel <szymon.mentel@erlang-solutions.com>
2015-04-07 15:50:28 +0200

    Plot handle_packet_in metric

--- scripts/exo_lager_to_gnuplot.py
+++ scripts/exo_lager_to_gnuplot.py
@@ -2,7 +2,8 @@
 import datetime as dt
 
 log_file = 'log/notice.log'
-count_file = 'packet_in.metrics'
+pkt_in_count_file = 'pkt_in.metrics'
+handle_pkt_in_mean_file = 'handle_pkt_in.metrics'
 
 
 def seconds_from_start(raw_time):
@@ -15,7 +16,7 @@ def seconds_from_start(raw_time):
         return 0
 
 
-with open(log_file) as f, open(count_file, 'w') as count:
+with open(log_file) as f, open(pkt_in_count_file, 'w') as pkt_in:
     prev_count = 0
     for line in f:
         if 'packet_in_count' in line:
@@ -25,5 +26,14 @@ with open(log_file) as f, open(count_file, 'w') as count:
             count_value = int(splitted[5].split(":")[1])
             calc_one_value = count_value - prev_count
             prev_count = count_value
-            count.write('%s %d %d \n' % (secs_from_start, count_value,
-                                         calc_one_value))
+            pkt_in.write('%d %d %d \n' % (secs_from_start, count_value,
+                                          calc_one_value))
+
+with open(log_file) as f, open(handle_pkt_in_mean_file, 'w') as handle_pkt_in:
+    for line in f:
+        if 'handle_packet_in_mean' in line:
+            splitted = line.split()
+            time = splitted[1].split('.')[0]
+            secs_from_start = seconds_from_start(time)
+            value = int(splitted[5].split(":")[1])
+            handle_pkt_in.write('%d %d \n' % (secs_from_start, value))
