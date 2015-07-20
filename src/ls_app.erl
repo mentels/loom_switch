43f46ae53587e2aca16c18d51d29c614373745aa
Szymon Mentel <szymon.mentel@erlang-solutions.com>
2015-07-20 10:44:03 +0200

    Set numbers of schedulers online

--- src/ls_app.erl
+++ src/ls_app.erl
@@ -10,7 +10,18 @@
 %% ===================================================================
 
 start(_StartType, _StartArgs) ->
+    set_schedulers(),
     ls_sup:start_link().
 
 stop(_State) ->
     ok.
+
+set_schedulers() ->
+    case application:get_env(ls, schedulers, default) of
+        default ->
+            ok;
+        Num when is_integer(Num) ->
+            erlang:system_flag(schedulers_online, Num),
+            lager:info([{ls, x}], "Set number of schedulers online to ~p",
+                       [Num])
+    end.
